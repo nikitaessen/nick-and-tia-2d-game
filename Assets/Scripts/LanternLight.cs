197482b72745da8891f2254eb5a0751693e5e29d
Ti M. <tim10@gmail.com>
2024-08-09 22:59:32 -0400

    Added lantern pickup effect

---
+++ Assets/Scripts/LanternLight.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternLight : MonoBehaviour
+{
+    public GameObject light;
+
+    void Start()
+    {
+        light.GetComponent<Light>().enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            
+            light.GetComponent<Light>().enabled = true;
+        }
+    }
+}
